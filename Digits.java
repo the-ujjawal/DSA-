774c96af3a0ecf17ee607d7c6666f9d8e7a3d8bb
the-ujjawal <uksah123123@gmail.com>
2025-08-17 12:49:56 +0530

    Printing digits of a number

---
+++ Digits.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Digits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number : ");
+        int n = sc.nextInt();
+
+        int count = 0;
+        int temp = n;
+        while(temp != 0){
+            temp = temp / 10;
+            count++;
+        }
+        int div = (int)Math.pow(10, count - 1);
+        while(div != 0){
+            int q = n / div;
+            System.out.println(q);
+
+            n = n % div;
+            div = div / 10;
+        }
+    }
+}
