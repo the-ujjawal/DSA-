68a91ddbac524274f941dabf397e94b908791cbe
the-ujjawal <uksah123123@gmail.com>
2026-02-05 22:26:27 +0530

    Addition of two numbers

---
+++ Basics/Add.java
@@ -0,0 +1,8 @@
+public class Add {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int sum = x + y;
+        System.out.println("The sum of"+ x + " and " +y+ " is:" + sum);
+    }
+}
