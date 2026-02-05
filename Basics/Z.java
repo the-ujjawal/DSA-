2142a693daea9bcf110353a2f2602d306fc6b94f
Ujjawal Kumar <uksah123123@gmail.com>
2026-02-05 22:29:33 +0530

    Delete Basics directory

--- Basics/Z.java
+++
@@ -1,9 +0,0 @@
-public class Z {
-    public static void main(String[] args) {
-        System.out.println("*****");
-        System.out.println("   *");
-        System.out.println("  *");
-        System.out.println(" *");
-        System.out.println("*****");
-    }
-}
