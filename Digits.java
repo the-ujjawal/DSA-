711b3133e76cce6acd0468af540e9942a2f3d1c1
the-ujjawal <uksah123123@gmail.com>
2026-03-11 16:22:55 +0530

    Printing the digits of a number

--- Digits.java
+++ Digits.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class digits {
+public class Digits {
      public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.print("Enter number : ");
