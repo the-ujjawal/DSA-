948998b6029ec0c7aa8911718d2908732d9bf250
the-ujjawal <uksah123123@gmail.com>
2026-04-05 18:57:17 +0530

    Identifying the given numbers are Pythagorean triplets or not

---
+++ PythagoreanTriplets.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class PythagoreanTriplets{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a : ");
+        int a = scn.nextInt();
+        System.out.println("Enter b : ");
+        int b = scn.nextInt();
+        System.out.println("Enter c : ");
+        int c = scn.nextInt();
+
+        int max = a;
+        if(b >= max){
+            max = b;
+        }
+        if(c >= max){
+        max = c;
+        }
+
+        if(max == a){
+            boolean flag = ((b*b+c*c)==(a*a));
+            System.out.println(flag);
+
+        }else if(max == b){
+            boolean flag = ((a*a+c*c)==(b*b));
+            System.out.println(flag);
+            
+        }else{
+            boolean flag = ((b*b+a*a)==(c*c));
+            System.out.println(flag);
+
+        }
+    }
+}
