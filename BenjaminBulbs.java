04a284cbca6818a8688b9944ba95daee712a968d
the-ujjawal <uksah123123@gmail.com>
2026-04-06 11:02:16 +0530

    Switching on n number of bulbs after n number of fluctuations

---
+++ BenjaminBulbs.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class BenjaminBulbs {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter number of bulbs : ");
+        int n = scn.nextInt();
+
+        for (int i = 1; i * i <= n; i++) {
+            System.out.println(i * i);
+        }
+    }
+}
