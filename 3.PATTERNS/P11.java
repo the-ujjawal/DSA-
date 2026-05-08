8bcb5a3fe581db19a27e7e8e2360ab1a651b5532
the-ujjawal <uksah123123@gmail.com>
2026-05-08 23:19:10 +0530

    Printing digits in triangular pattern

---
+++ 3.PATTERNS/P11.java
@@ -0,0 +1,27 @@
+
+public class P11 {
+
+    public static void main(String[] args) {
+
+        int n = 6;
+        int sp = n - 1;
+
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= sp; col++) {
+                System.out.print("-\t");
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + "\t");
+            }
+            int temp = row - 1;
+            if (row > 1) {
+                for (int col = 1; col <= row - 1; col++) {
+                    System.out.print(temp + "\t");
+                    temp--;
+                }
+            }
+            sp--;
+            System.out.println();
+        }
+    }
+}
