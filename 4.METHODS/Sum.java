4f91a3af2ae16a08a973a7f1dfe5881aaa1254aa
the-ujjawal <uksah123123@gmail.com>
2026-05-10 08:45:38 +0530

    Printing sum by calling method add(int x, int y

---
+++ 4.METHODS/Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    
+    public static int add(int x, int y) {
+        return x + y;
+    }
+    public static void main(String[] args) {
+        int sum = add(5, 8);
+        System.out.println("Sum "+sum);
+    }
+}
