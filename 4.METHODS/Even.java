7d799d89999215cd211db5073f65dd741af40b18
the-ujjawal <uksah123123@gmail.com>
2026-05-10 09:13:48 +0530

    Printing even or not

--- 4.METHODS/Even.java
+++ 4.METHODS/Even.java
@@ -1,14 +1,23 @@
 public class Even {
     
-    public static void IsEven(int n) {
-        if(n%2==0){
-            System.out.println("Even");
-        }
-        else{
-            System.out.println("Odd");
-        }
+    // public static void IsEven(int n) {
+    //     if(n%2==0){
+    //         System.out.println("Even");
+    //     }
+    //     else{
+    //         System.out.println("Odd");
+    //     }
+    // }
+    // public static void main(String[] args) {
+    //     IsEven(22);
+    // }
+
+    public static boolean IsEven(int n) {
+        return n % 2 == 0;
     }
+
     public static void main(String[] args) {
-        IsEven(22);
+        boolean ans = IsEven(21);
+        System.out.println(ans);
     }
 }
