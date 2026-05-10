2474fbc350137dfc5c1f95d6299af2e87c078c84
the-ujjawal <uksah123123@gmail.com>
2026-05-10 08:58:32 +0530

    Printing even

---
+++ 4.METHODS/Even.java
@@ -0,0 +1,14 @@
+public class Even {
+    
+    public static void IsEven(int n) {
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+    public static void main(String[] args) {
+        IsEven(22);
+    }
+}
