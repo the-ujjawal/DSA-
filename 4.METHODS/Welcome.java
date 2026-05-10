94ee797bff0d8ae79faeeb8d07a74ea6f76312ef
the-ujjawal <uksah123123@gmail.com>
2026-05-10 08:35:45 +0530

    Printing WelcomeMessage

---
+++ 4.METHODS/Welcome.java
@@ -0,0 +1,10 @@
+public class Welcome{
+
+    public static void WelcomeMessage(){
+        System.out.println("Welcome to Java Programming");
+    }
+
+    public static void main(String[] args) {
+        WelcomeMessage();
+    }
+}
