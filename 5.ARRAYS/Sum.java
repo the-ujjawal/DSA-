22db6e3cb25bb79c30d5e689f9a34bf53ef94b42
the-ujjawal <uksah123123@gmail.com>
2026-05-13 00:04:20 +0530

    Sum of elements of array

--- 5.ARRAYS/Sum.java
+++ 5.ARRAYS/Sum.java
@@ -2,19 +2,19 @@
 import java.util.Scanner;
 
 public class Sum {
+
     public static void main(String[] args) {
 
         //1D Array
-
+        
         // int ar[] = {2,3,5,7};
         // int sum = 0;
-
         // for (int i = 0; i < ar.length; i++) {
         //     sum = sum + ar[i];
         // }
         // System.out.println("Sum of elements : "+sum);
 
-        
+
         //2D Array
 
         Scanner scn = new Scanner(System.in);
@@ -23,11 +23,11 @@ public class Sum {
 
         for (int i = 0; i < ar.length; i++) {
             for (int j = 0; j < ar[i].length; j++) {
-                System.out.print("Enter value at index "+i+" and "+j+" : ");
+                System.out.print("Enter value at index " + i + " and " + j + " : ");
                 ar[i][j] = scn.nextInt();
                 sum = sum + ar[i][j];
             }
         }
-        System.out.println("Sum of elements : "+sum);
+        System.out.println("Sum of elements : " + sum);
     }
 }
