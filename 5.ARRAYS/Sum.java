6e6fd4d205a497dbd63be81c856ac76c87ba64e3
the-ujjawal <uksah123123@gmail.com>
2026-05-12 23:56:09 +0530

    Minimu of elements of 1D array

--- 5.ARRAYS/Sum.java
+++ 5.ARRAYS/Sum.java
@@ -1,10 +1,32 @@
+
+import java.util.Scanner;
+
 public class Sum {
     public static void main(String[] args) {
-        int ar[] = {2,3,5,7};
+
+        //1D Array
+
+        // int ar[] = {2,3,5,7};
+        // int sum = 0;
+
+        // for (int i = 0; i < ar.length; i++) {
+        //     sum = sum + ar[i];
+        // }
+        // System.out.println("Sum of elements : "+sum);
+
+        
+        //2D Array
+
+        Scanner scn = new Scanner(System.in);
+        int ar[][] = new int[2][3];
         int sum = 0;
 
         for (int i = 0; i < ar.length; i++) {
-            sum = sum + ar[i];
+            for (int j = 0; j < ar[i].length; j++) {
+                System.out.print("Enter value at index "+i+" and "+j+" : ");
+                ar[i][j] = scn.nextInt();
+                sum = sum + ar[i][j];
+            }
         }
         System.out.println("Sum of elements : "+sum);
     }
