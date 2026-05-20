47f49802e0ba278e579494fcbc8dcfa464dd1146
the-ujjawal <uksah123123@gmail.com>
2026-05-20 13:09:42 +0530

    Practicing basics of OOPS

---
+++ Bank.java
@@ -0,0 +1,27 @@
+public class Bank {
+
+    public static void main(String[] args) {
+
+        BankAccount b1 = new BankAccount();
+
+        b1.balance = 1000;
+
+        b1.deposit(500);
+
+        b1.showBalance();
+    }
+}
+
+class BankAccount {
+
+    //String accountHolder;
+    int balance;
+
+    void deposit(int amount) {
+        balance = balance + amount;
+    }
+
+    void showBalance() {
+        System.out.println(balance);
+    }
+}
